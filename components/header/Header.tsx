7c4c646bd90626fecd2e7b8352582dddf4cae834
Kirill <kirill2004pro1000@mail.ru>
2023-06-06 10:44:48 +0300

    finish

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -27,7 +27,7 @@ export const Header = () => {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-[#3e3e3e]">
+      <nav className="bg-white h-[56px] border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-[#3e3e3e]">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link href="/" className="flex items-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
