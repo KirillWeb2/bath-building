f52ed4f4e116634f7ffb7b86f1c2dac3bc5c9a2b
Kirill <kirill2004pro1000@mail.ru>
2023-06-07 11:34:12 +0300

    finish 3

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -39,7 +39,7 @@ export const Header = () => {
               onClick={toggleBurger}
               data-collapse-toggle="mobile-menu-2"
               type="button"
-              className="inline-flex items-center mr-4 p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-[#6c6c6c] dark:focus:ring-gray-600"
+              className="absolute right-[10px] top-[10px] inline-flex items-center mr-0 p-2 ml-1 text-sm text-gray-500 rounded-lg lg:mr-0 lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-[#6c6c6c] dark:focus:ring-gray-600"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
